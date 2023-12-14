77d9f52614b8310342cc849b6a10e4d4dc2d75f7
akhil1427 <akhilkonda61@gmail.com>
2023-12-14 12:05:05 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class EmployeeTable(models.Model):
-    Empno=models.IntegerField(primary_key=True)
+    Empno=models.IntegerField()
     Ename=models.CharField(max_length=50)
     Job=models.CharField(max_length=20)
     Mgr=models.IntegerField()
@@ -18,7 +18,7 @@ class DEPT(models.Model):
     D_Name=models.CharField(max_length=20)
     Loc=models.CharField(max_length=30)
     def __str__(self):
-        return  str (self.pk)
+        return  self.D_Name
 
 class SalGrade(models.Model):
     Grade=models.IntegerField()
--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
+
+def display_tables(request):
+    ETOO=EmployeeTable.objects.all()
+    d={'topics':ETOO}
+    return render(request,'display_tables.html',d)
+
